a416b97e47cae82c0e4545fcc73404c93157f0d4
JonasTeich <jonasteichrueb@gmail.com>
2021-09-30 18:21:40 +0200

    Adding PWA

--- signup/signup.js
+++ signup/signup.js
@@ -11,6 +11,10 @@ const SUPABASE_URL = 'https://hkfomhpjagnnioxqgane.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMjgzMDY5NCwiZXhwIjoxOTQ4NDA2Njk0fQ.uYldi0Q_JK7bfYYjG98YctlmxQ7MWZfKqnvW_yalSAw'
 var supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../service-worker.js')
+}
+
 signUpBtn.addEventListener('click', () => {
   signUpProcess()
 })
