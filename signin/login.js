38657a1360fe4d709d98a86f7ed6c0d2f571c3a5
JonasTeich <jonasteichrueb@gmail.com>
2021-10-06 09:58:33 +0200

    Fixing login bug

--- signin/login.js
+++ signin/login.js
@@ -8,11 +8,14 @@ const SUPABASE_URL = 'https://hkfomhpjagnnioxqgane.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMjgzMDY5NCwiZXhwIjoxOTQ4NDA2Njk0fQ.uYldi0Q_JK7bfYYjG98YctlmxQ7MWZfKqnvW_yalSAw'
 var supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
 
-document.addEventListener("DOMContentLoaded", () => {
-  if (supabase.auth.user() !== null) {
+setUp()
+
+async function setUp () {
+  const user = await supabase.auth.user()
+  if (user !== null) {
     document.location.href = '../selectplan'
   }
-})
+}
 
 signUpBtn.addEventListener('click', () => {
   document.location.href = "../signup"
