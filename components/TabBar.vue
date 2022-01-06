822184e8eebc9d1b17654cb949f530dc91783c5d
Jonas <jonasteichrueb@gmail.com>
2022-01-06 17:04:08 +0100

    removing mission

--- components/TabBar.vue
+++ components/TabBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-between fixed left-0 bottom-0 w-full bg-gray-100 h-16 p-4 px-2 z-20">
     <tab-bar-link to="/home" icon="home">Home</tab-bar-link>
-    <tab-bar-link to="/mission" icon="compass">Mission</tab-bar-link>
+    <!-- <tab-bar-link to="/mission" icon="compass">Mission</tab-bar-link> -->
     <tab-bar-link to="/plans" icon="book-reader">Pläne</tab-bar-link>
     <tab-bar-link to="/bible" icon="bible">Bibel</tab-bar-link>
     <tab-bar-link to="/pray" icon="praying-hands">Gebet</tab-bar-link>
@@ -9,3 +9,13 @@
     <!-- <tab-bar-link to="/songs" icon="music">Lieder</tab-bar-link> -->
   </div>
 </template>
+
+<script>
+export default {
+  beforeMount () {
+    if (this.$route.pathname === undefined) {
+      this.$router.push('/home')
+    }
+  }
+}
+</script>
